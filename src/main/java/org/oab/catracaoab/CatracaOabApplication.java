1b2e79317bd38d7a7261e483156d9500fe4343ec
GustavoMR <gustavomrodrigues11@gmail.com>
2025-06-26 19:18:08 -0300

    init: initial commit

---
+++ src/main/java/org/oab/catracaoab/CatracaOabApplication.java
@@ -0,0 +1,13 @@
+package org.oab.catracaoab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatracaOabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatracaOabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/oab/catracaoab/CatracaOabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oab.catracaoab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatracaOabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
